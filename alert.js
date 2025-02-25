da94473cb9f22af710718b2aa0abf0dc3d1b0d07
Supriya <supriyasantoshsawant01@gmail.com>
2025-02-25 20:49:03 +0530

    Updated messageBox.js

--- alert.js
+++ alert.js
@@ -1,1 +1,4 @@
-
+var len = prompt("Enter your message here:");
+var a = 140-len.length;
+alert("You have written "+len.length+" characters, you have "+a+" characters left.");
+alert(len.slice(0,140));
