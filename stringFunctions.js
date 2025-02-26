d0846c2a995a78b31091959a2148a8f1ecb0cb30
Supriya <supriyasantoshsawant01@gmail.com>
2025-02-26 17:42:33 +0530

    Updated stringFunctions.js

--- stringFunctions.js
+++ stringFunctions.js
@@ -1,1 +1,5 @@
-
+var name = prompt("What is your name?");
+var firstChar = name.slice(0,1).toUpperCase();
+var otherChar = name.slice(1,name.length).toLowerCase();
+name = firstChar + otherChar;
+alert("Hello, "+name);
