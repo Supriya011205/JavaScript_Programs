400cbe92d33936af81a713de2cced321caabb579
Supriya <supriyasantoshsawant01@gmail.com>
2025-02-27 21:15:37 +0530

    Updated leapYear.js

--- leapYear.js
+++ leapYear.js
@@ -1,1 +1,11 @@
+function isLeap(year) {
+  if(year%4 === 0){
+      if(year%100 !== 0 || year%400 === 0){
+          console.log("Leap year.");
+      }
+  } else {
+    console.log("Not leap year.");
+  }
+}
 
+isLeap(2024);
