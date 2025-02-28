6af919f4083b1c350d4bbe3751b69fa7f95b5ffc
Supriya <supriyasantoshsawant01@gmail.com>
2025-02-28 20:09:36 +0530

    Updated and rename random1.js to guestList.js

---
+++ guestList.js
@@ -0,0 +1,8 @@
+guestList = ["Lisa","Riya","Ben","Jack","Jane"];
+var guestName = prompt("Enter your name");
+
+if(guestList.includes(guestName)){
+  alert("Welcome!");
+} else {
+  alert("Sorry! You are not invited")
+}
