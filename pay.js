d42cf9a93220a918e6de95067ad86e53357ab286
Supriya <supriyasantoshsawant01@gmail.com>
2025-03-01 12:59:21 +0530

    Updated pay.js

--- pay.js
+++ pay.js
@@ -1,1 +1,10 @@
+var array = ["May", "Ben", "Jenny", "Michael", "Chloe"];
 
+function whosPaying(names) {
+    var r = Math.random();
+    r = Math.floor(r * names.length);
+    return names[r]+" is going to buy lunch today!";
+    return s;
+}
+
+console.log(whosPaying(array));
