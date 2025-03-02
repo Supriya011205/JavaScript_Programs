2bf4a38a48efbdd47fdd9049b29bd86a14b4f7e0
Supriya <supriyasantoshsawant01@gmail.com>
2025-03-02 13:56:37 +0530

    Updated random2.js

--- random2.js
+++ random2.js
@@ -1,1 +1,10 @@
+prompt("What is your name: ");
+prompt("What is their name: ");
+var n = Math.random();
+n = Math.floor(n*100) + 1;
 
+if(n > 75){
+  alert("Your love score is high "+n);
+} else {
+  alert("Your love score is "+n)
+}
