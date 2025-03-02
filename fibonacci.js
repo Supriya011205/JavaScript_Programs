b1e037a73005cd6631130ae7b4cc26db094b5d95
Supriya <supriyasantoshsawant01@gmail.com>
2025-03-02 14:00:11 +0530

    Updated fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,1 +1,17 @@
+function fibonacciGenerator (n) {
+    var output = [];
+    var count = 0;
+    var firstfib = 0;
+    var nextfib = 1;
+    var totalfib = 0;
+    for (var i = 0; i < n; i++) {
+        output.push(firstfib);
+        
+        totalfib = firstfib + nextfib;
+        firstfib = nextfib;
+        nextfib = totalfib;
+    }
+    return output;
+}
 
+console.log(fibonacciGenerator(10));
