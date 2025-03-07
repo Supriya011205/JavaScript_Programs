353bf00760e81d31c63cf716f9aac7daf187fbb3
Supriya <supriyasantoshsawant01@gmail.com>
2025-03-07 20:33:12 +0530

    Updated life_in_weeks.js

--- life_in_weeks.js
+++ life_in_weeks.js
@@ -1,1 +1,11 @@
+function lifeInWeeks(age) {
+    var age_left = 90 - age;
+    var days = 365*age_left;
+    var weeks = 52*age_left;
+    var months = 12*age_left;
+    
+    console.log("You have "+days+" days, "+weeks+" weeks, and "+months+" months left.");
 
+}
+
+lifeInWeeks(18);
