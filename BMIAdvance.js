f70fd6832709d3680004f587aec1f17fc74eb424
Supriya <supriyasantoshsawant01@gmail.com>
2025-03-10 20:34:59 +0530

    Update 2 BMIAdvance.js

--- BMIAdvance.js
+++ BMIAdvance.js
@@ -1,1 +1,8 @@
-   
+
+function bmiCalculator (weight, height) {
+    var bmi = weight/(height*height);
+    bmi = Math.floor(bmi);
+    var interpretation;
+    if(bmi < 18.5){
+        interpretation = "Your BMI is "+bmi+", so you are underweight.";
+    }   
