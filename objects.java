b08542c8bdd641cf3013afa1b42b393aa82f8a56
Supriya <supriyasantoshsawant01@gmail.com>
2025-03-18 13:25:47 +0530

    Update objects.java

--- objects.java
+++ objects.java
@@ -1,1 +1,10 @@
-
+var houseKeeper1 = {
+    name: "Meera",
+    age: 2,
+    hoursOfWorking: 5,
+    daysOfWorkimg:5,
+    job: ["cleaning","cooking","washing"],
+    yearsOfExperience: 12
+}
+console.log(houseKeeper1.name);
+console.log(houseKeeper);
